6b1442c428b9c75ad774992bb071192149cbe79e
noku2la58 <4275819@myuwc.ac.za>
2022-05-13 15:15:22 +0200

    Add project files.

---
+++ MrD/MrD/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MrD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var narPage = new NavigationPage(new MainPage());
+            MainPage = narPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
